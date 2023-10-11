05a5790e4326b2541c57eef153d7bdcc7cc5b272
vannnnnnnnnnnny <116868412+vannnnnnnnnnnny@users.noreply.github.com>
2023-10-11 22:56:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,18 +25,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "ate.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "TURN ON LIGHTS",
+        play: "ENTER",
+        bannar_coming: "ANG DECORATION",
+        balloons_flying: "ANG BALLOON ATE",
+        cake_fadein: "IMONG CAKE",
+        light_candle: "ANG KANDILA",
+        wish_message: "HAPPY BIRTHDAY ATE HYDZ",
+        story: "Wishing you a very happy birthday!” “Today is the day you were brought into this world to be a blessing and inspiration to the people around you! Wish you a very happy birthday!” “Sending you smiles and happiness for every moment of your special day.",
     }
 };
